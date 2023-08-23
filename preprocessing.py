80ff302254f69ad47b1bb93ed6e7e43290fd1f65
Atharva Tikhe <atharvatikhe22@gmail.com>
2023-08-23 16:59:20 +0530

    kmeans

--- preprocessing.py
+++ preprocessing.py
@@ -29,7 +29,7 @@ img_hsv[:, :, 2] = cv.equalizeHist(img_hsv[:, :, 2])
 # convert image back from HSV to RGB
 img = cv.cvtColor(img_hsv, cv.COLOR_HSV2BGR)
 
-cv.imwrite("he.jpg", img)
+# cv.imwrite("he.jpg", img)
 
 
 #```
@@ -51,5 +51,39 @@ cv.imwrite("he.jpg", img)
 
 med_blur_img = cv.medianBlur(img, 5)
 
-cv.imwrite("med_blur.jpg", med_blur_img)
+# cv.imwrite("med_blur.jpg", med_blur_img)
+
+
+"""IMAGE SEGMENTATION 
+1. Image is converted to RGB
+2. Reshape to 2D
+3. K-means clustering
+"""
+
+rgb_image = cv.cvtColor(med_blur_img, cv.COLOR_BGR2RGB)
+cv.imwrite("rgb_image.jpg",rgb_image)
+
+pixel_vals = rgb_image.reshape((-1,3))
+
+pixel_vals = np.float32(pixel_vals)
+
+#the below line of code defines the criteria for the algorithm to stop running,
+#which will happen is 100 iterations are run or the epsilon (which is the required accuracy)
+#becomes 85%
+criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 100, 0.85)
+
+# then perform k-means clustering with number of clusters defined as 3
+#also random centres are initially choosed for k-means clustering
+k = 3
+retval, labels, centers = cv.kmeans(pixel_vals, k, None, criteria, 10, cv.KMEANS_RANDOM_CENTERS)
+
+# convert data into 8-bit values
+centers = np.uint8(centers)
+segmented_data = centers[labels.flatten()]
+
+# reshape data into the original image dimensions
+segmented_image = segmented_data.reshape((rgb_image.shape))
+
+cv.imwrite("segmented.jpg", segmented_image)
+
 
