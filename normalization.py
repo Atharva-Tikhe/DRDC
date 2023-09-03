44bee62e3b79baa69123ae9483d2c9375d33d874
Atharva Tikhe <atharvatikhe22@gmail.com>
2023-09-03 17:36:05 +0530

    update: normalization

--- normalization.py
+++ normalization.py
@@ -1,10 +1,8 @@
 import cv2 as cv 
-
+import numpy as np
 img = cv.imread("test_images/graham.jpg")
 gray_img = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
 # img = cv.resize(img, (512,512))
 mean, stddev = cv.meanStdDev(gray_img)
 
 norm_img = (img - mean)/stddev
-
-cv.imwrite("test_images/normalized.jpg" , norm_img)
